3faa1b42bf17fbc4990dab3fc316142d4e3584cc
Harm Brouwer <me@hbrouwer.eu>
2017-03-28 14:02:49 +0200

    added dimension reduction algorithms

---
+++ reduce/reduce_compl.r
@@ -0,0 +1,147 @@
+##
+#    ,--.,--.
+#  ,-|  |`--' ,---.  ,---.  ,--,--. ,---. ,---.
+# ' .-. |,--.(  .-' | .-. |' ,-.  || .--'| .-. :
+# \ `-' ||  |.-'  `)| '-' '\ '-'  |\ `--.\   --.
+#  `---' `--'`----' |  |-'  `--`--' `---' `----'
+#                   `--'
+#     "To wander, to roam, move about."
+#
+# Copyright 2014-2017 Harm Brouwer <me@hbrouwer.eu>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+##
+
+###########################################################################
+###########################################################################
+
+if (!exists("model"))
+        stop("'model' not set")
+if (!exists("model_fb")) {
+        model_nm <- tail(strsplit(model, "/")[[1]], 1)
+        model_fb <- paste(model, model_nm, sep = "/")
+}
+
+###########################################################################
+###########################################################################
+
+obs_file <- paste(model_fb, ".observations", sep = "")
+
+# read observations and vectors
+cat("Reading observations ...\n", file = stderr())
+df.obs <- read.csv(obs_file, sep = " ", head = TRUE, check.names = FALSE)
+
+# convert data frame to matrix
+df.mtx <- as.matrix(df.obs)
+
+###########################################################################
+####                C O M P E T I T I V E   L A Y E R                  ####
+###########################################################################
+
+##
+# Transforms observation vectors S into situation vectors u(a) using
+# a Competitive Layer algorithm: 
+#
+#         e=1 e=2 e=3     e=N                     e=1 e=2 e=3 ... e=4
+# k = 1 [  1   1   0  ...  0  ]        u(a) = 1 [ .00 .04 .57 ... .57 ]
+# k = 2 [  1   0   1  ...  1  ]  -->>  u(a) = 2 [ .99 .99 .00 ... .00 ]
+# k = 3 [  0   0   0  ...  0  ]  ==>>  u(a) = 3 [ .00 .00 .36 ... .00 ]
+# ..... [  .   .   .       .  ]  -->>  ........ [ ... ... ... ... ... ]
+# k = M [  0   0   1  ...  0  ]        u(a) = m [ .00 .99 .00 ... ... ]
+#
+# Each unit i of a Competitive Layer is assigned a weight vector, with
+# a weight for each basic event e in the microworld. In addition, each unit
+# i is assigned a single bias unit b_i.  Weights are initially set to 0.5,
+# and biases to 1. The resulting Competitive Layer is then trained for
+# N epochs. During each epoch, the following procedure is repeated for each
+# obsveration Sk:
+#
+# (1) For each unit i, the cityblock distance is determined between the
+# weight vector assigned to unit i, and observation Sk:
+#
+#          dist(u_i,Sk) = sum_a|u_i(a) - Sk(a)|
+#
+# (2) The unit w with the shortest, biased distance to Sk(a) is determined:
+#
+#          w = argmin_i(dist(u_i,Sk) - b_i)
+#
+# (3) The weight vector of unit w (the winner) is updated:
+#
+#          Du_w = alpha * (Sk - u_w)
+#
+#     where alpha is a learning rate parameter.
+#
+# (4) The bias of unit w (the winner) is decreased (to a minimum of 1):
+#
+#          Db_w = beta * b_w(1 - b_w)
+#
+#     where beta is a learning rate parameter.
+#
+# (5) The biases of all other units (i != w; the losers) are increased:
+#
+#          Db_i = beta * b_i
+#
+# The above is adapted from:
+#
+# Frank, S. L., Haselager, W. F. G., and van Rooij, I. (2009). Connectionist
+#     semantic systematicity. Cognition, 110(3):358–379.
+##
+reduce <- function(dims, epochs = 20, alpha = 0.1, alpha_sf = 0.1, beta = 0.00008, beta_sf = 0.1, write = FALSE)
+{
+        biases  <- rep(1.0, dims)
+        weights <- matrix(rep(0.5, dims * ncol(df.mtx)), dims, ncol(df.mtx))
+
+        epoch <- 1
+        while (epoch <= epochs) {
+                cat(paste("Epoch:", epoch, "Alpha:", alpha, "AlphaSF:", alpha_sf, "Beta:", beta, "BetaSF:", beta_sf), file = stderr())
+                cat("\n")
+
+                for (i in 1 : nrow(df.mtx)) {
+                        dv <- c()
+                        for (j in 1 : nrow(weights)) {
+
+                                ## (1): dist(u_i,Sk) = sum_a|u_i(a) - Sk(a)|
+                                dv <- c(dv, sum(abs(df.mtx[i,] - weights[j,]) - biases[j]))
+
+                                ## (2): w = argmin_i(dist(u_i,Sk) - b_i)
+                                w  <- which.min(dv)
+
+                                ## (3): Du_w = alpha * (Sk - u_w)
+                                weights[w,] <- weights[w,] + alpha * (df.mtx[w,] - weights[w,])
+
+                                ## (4): Db_w = beta * b_w(1 - b_w)
+                                biases[w]  <- min(1, biases[w] + beta  * biases[w] * (1 - biases[w]))
+
+                                ## (5): Db_i = beta * b_i
+                                biases[-w] <- biases[-w] + beta * biases[-w]
+                        }
+                }
+
+                epoch <- epoch + 1;
+
+                # sacle alpha and beta
+                alpha <- alpha - alpha_sf * alpha;
+                beta  <- beta  - beta_sf  * beta;
+        }
+
+        # write weights (if required)
+        if (write) {
+                vec_file <- paste(model_fb, ".vectors", sep = "")
+                cat(paste("Wrote:", vec_file, "\n"), file = stderr())
+                colnames(weights) <- colnames(df.mtx)
+                write.table(weights, vec_file, quote = FALSE, sep = " ",
+                            col.names = TRUE, row.names = FALSE)
+        }
+
+        weights
+}
---
+++ reduce/reduce_subset.r
@@ -0,0 +1,186 @@
+##
+#    ,--.,--.
+#  ,-|  |`--' ,---.  ,---.  ,--,--. ,---. ,---.
+# ' .-. |,--.(  .-' | .-. |' ,-.  || .--'| .-. :
+# \ `-' ||  |.-'  `)| '-' '\ '-'  |\ `--.\   --.
+#  `---' `--'`----' |  |-'  `--`--' `---' `----'
+#                   `--'
+#     "To wander, to roam, move about."
+#
+# Copyright 2014-2017 Harm Brouwer <me@hbrouwer.eu>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+##
+
+###########################################################################
+###########################################################################
+
+if (!exists("model"))
+        stop("'model' not set")
+if (!exists("model_fb")) {
+        model_nm <- tail(strsplit(model, "/")[[1]], 1)
+        model_fb <- paste(model, model_nm, sep = "/")
+}
+
+###########################################################################
+###########################################################################
+
+obs_file <- paste(model_fb, ".observations", sep = "")
+
+# read observations and vectors
+cat("Reading observations ...\n", file = stderr())
+df.obs <- read.csv(obs_file, sep = " ", head = TRUE, check.names = FALSE)
+
+# convert data frame to matrix
+df.mtx <- as.matrix(df.obs)
+
+##
+# Reduce the dimensionality of an m x n observation matrix X using subset
+# sampling. The following procedure is repeated for "epochs" epochs to
+# arrive at an k x n matrix X' (where k < m) that maximally reflects the
+# knowledge encoded in the original matrix X:
+# 
+#     (1) Take a subset of k rows of matrix X, and call it X'
+#
+#     (2) Check if all columns of matrix X' are informative, and if
+#         the reduced matrix encodes the same hard constraints as
+#         the unreduced matrix, otherwise skip to the next epoch
+#
+#     (3) Compute the similarity between X and X' on the basis of
+#         the comprehension scores in X and X'.
+#
+#     (4) If X' is the best approximation of X so far, store it
+#
+#     (5) Run next epoch, and rerun from step (1)
+#
+#     (6) If we have reached "epochs", return the best X' found
+##
+reduce <- function(dims, epochs, write = FALSE)
+{
+        compVec <- comprehension_vector(df.mtx)
+
+        reducedMtx <- matrix()
+        reducedSim <- 0
+
+        epoch <- 1
+        while (epoch <= epochs) {
+                cat(paste("Epoch:", epoch), file = stderr())
+                sdims <- sample(1 : nrow(df.mtx), dims, replace = FALSE)
+                
+                trialMtx <- df.mtx[sdims,]
+                trialCompVec <- comprehension_vector(trialMtx)
+                if (!all_informative_vectors(trialMtx)
+                    | !equal_hard_constraints(compVec, trialCompVec)) {
+                        cat("\tBad sample ... Skipping epoch!\n", file = stderr())
+                        next
+                }
+                trialSim <- similarity(compVec, comprehension_vector(trialMtx))
+                cat(paste("\tSim:", trialSim), file = stderr())
+
+                if (epoch == 1) {
+                        reducedMtx <- trialMtx
+                        reducedSim <- trialSim
+                } else {
+                        if (trialSim > reducedSim) {
+                                reducedMtx <- trialMtx
+                                reducedSim <- trialSim
+                                cat(" ***", file = stderr())
+                        }
+                }
+
+                cat("\n", file = stderr())
+                epoch <- epoch + 1
+        }
+
+        cat(paste("Sim: ", reducedSim, "\n"), file = stderr())
+
+        # write matrix (if required)
+        if (write) {
+                vec_file <- paste(model_fb, ".vectors", sep = "")
+                cat(paste("Wrote:", vec_file, "\n"), file = stderr())
+                colnames(reducedMtx) <- colnames(df.mtx)
+                write.table(reducedMtx, vec_file, quote = FALSE, sep = " ",
+                            col.names = TRUE, row.names = FALSE)
+        }
+
+        reducedMtx
+}
+
+# equal hard constraints
+equal_hard_constraints <- function(cv1, cv2)
+{
+        all(replace(cv1, cv1 > -1 & cv1 < 1, 0) == replace(cv2, cv2 > -1 & cv2 < 1, 0))
+}
+
+# returns similarity between two vectors
+similarity <- function(v1, v2)
+{
+        cor(v1, v2)
+}
+
+# returns whether all matrix columns are informative
+all_informative_vectors <- function(mtx)
+{
+        all(colSums(mtx) > 0)
+}
+
+# returns a vector of comprehension scores
+comprehension_vector <- function(mtx) 
+{
+        cv <- c()
+        
+        for (i in 1 : ncol(mtx)) {
+                for (j in 1 : ncol(mtx)) {
+                        cv <- c(cv, comprehension_score(mtx[,i], mtx[,j]))
+                }
+        }
+
+        cv
+}
+
+# prior probability
+prior_prob <- function(a)
+{
+        sum(a) / length(a)
+}
+
+# conjunction probability
+conj_prob <- function(a, b)
+{
+        if (all(a == b)) {
+                return(prior_prob(a))
+        } else {
+                return(sum(a * b) / length(a))
+        }
+}
+
+# conditional probability
+cond_prob <- function(a, b)
+{
+        conj_prob(a, b) / prior_prob(b)
+}
+
+# comprehension score
+comprehension_score <- function(a, b)
+{
+        pr_ab <- cond_prob(a, b)
+        pr_a <- prior_prob(a)
+
+        if (pr_ab > pr_a) {
+                cs <- (pr_ab - pr_a) / (1.0 - pr_a)
+        } else {
+                cs <- (pr_ab - pr_a) / pr_a
+        }
+
+        cs
+}
